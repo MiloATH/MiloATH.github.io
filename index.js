f0c3cce11ab4882748ddb0400b682fca7f7e20b5
Milo Hartsoe <milo.red.duck@gmail.com>
2017-01-08 01:16:38 +0000

    Add mail link

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+//Anti spam
+$(document).ready(function(){
+	var gmail = ["m","o","c",".","l","i","a","m","g"];
+	gmail.reverse();
+	var arr = ["e","o","s","t","r","a","h","o","l","i","m"];
+	arr.reverse();
+	var atSymbol = String.fromCharCode(64);
+	var m = "mail" + "to:" + arr.join("") + atSymbol + gmail.join("");
+	console.log(m);
+	$('#antispam').attr("href",m);
+});
+
 $('a[href*="#"]:not([href="#"])').click(function() {
 	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 		var target = $(this.hash);
