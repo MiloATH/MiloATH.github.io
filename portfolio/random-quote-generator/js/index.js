9d7685e45932ef8380164a0fc2c3b5d077363ad4
Milo Hartsoe <milo.red.duck@gmail.com>
2017-05-23 21:03:00 -0400

    Fixes json parsing bug in random quote project. Adds Voting App project and URL Shortener Project. Formats indenting of code in js of random quote project.

--- portfolio/random-quote-generator/js/index.js
+++ portfolio/random-quote-generator/js/index.js
@@ -2,63 +2,60 @@ var getQuote = false;
 
 //For view on iframe
 function iframe() {
-	$(".text").slideUp(400, function() {
-		$('#quote').text(" There's no place like home.");
-		$('#author').text(" -The Wizard of Oz");
-		$("#quote").addClass("fa-quote-left");
-	}).slideDown(400);
-	$(".loading").attr("style","visibility:hidden;");
-	tweetLink = "https://twitter.com/intent/tweet?text=" + encodeURI("There's no place like home. -The Wizard of Oz");
+    $(".text").slideUp(400, function() {
+        $('#quote').text(" There's no place like home.");
+        $('#author').text(" -The Wizard of Oz");
+        $("#quote").addClass("fa-quote-left");
+    }).slideDown(400);
+    $(".loading").attr("style", "visibility:hidden;");
+    tweetLink = "https://twitter.com/intent/tweet?text=" + encodeURI("There's no place like home. -The Wizard of Oz");
 }
 
 $(document).ready(function() {
-	try {
-		if (window.self !== window.top) {
-			iframe();
-		}
-		else {
-			newQuote();
-		}
-	}
-	catch (e) {
-		iframe();
-	}
+    try {
+        if (window.self !== window.top) {
+            iframe();
+        } else {
+            newQuote();
+        }
+    } catch (e) {
+        iframe();
+    }
 });
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 
 function tweet() {
-	console.log(tweetLink);
-	var feats = "location=yes,height=450,width=520,scrollbars=yes,status=yes";
-	var win = window.open(tweetLink, "_blank", feats);
+    console.log(tweetLink);
+    var feats = "location=yes,height=450,width=520,scrollbars=yes,status=yes";
+    var win = window.open(tweetLink, "_blank", feats);
 }
 
 function newQuote() {
-	$(".loading").css({
-		"visibility": "visible",
-		"animation": "rot .5s linear infinite"
-	});
-	if (!getQuote) {
-		getQuote = true;
-		$.ajax({
-			headers: {
-				"X-Mashape-Key": "tEthxoH7UpmshJEIan8MucKd4spCp1PYPjujsn5GcQvoK0eryB",
-				Accept: "application/json",
-				"Content-Type": "application/x-www-form-urlencoded"
-			},
-			url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-			success: function(response) {
-				var res = JSON.parse(response);
-				$(".text").slideUp(400, function() {
-					$('#quote').text(" " + res.quote);
-					$('#author').text("-" + res.author);
-					$("#quote").addClass("fa-quote-left");
-				}).slideDown(400);
-				tweetLink = "https://twitter.com/intent/tweet?text=" + encodeURI(res.quote + " -" + res.author);
-				getQuote = false;
-				$(".loading").css({
-					"visibility": "hidden",
-				});
-			}
-		});
-	}
+    $(".loading").css({
+        "visibility": "visible",
+        "animation": "rot .5s linear infinite"
+    });
+    if (!getQuote) {
+        getQuote = true;
+        $.ajax({
+            headers: {
+                "X-Mashape-Key": "tEthxoH7UpmshJEIan8MucKd4spCp1PYPjujsn5GcQvoK0eryB",
+                Accept: "application/json",
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+            success: function(res) {
+                $(".text").slideUp(400, function() {
+                    $('#quote').text(" " + res.quote);
+                    $('#author').text("-" + res.author);
+                    $("#quote").addClass("fa-quote-left");
+                }).slideDown(400);
+                tweetLink = "https://twitter.com/intent/tweet?text=" + encodeURI(res.quote + " -" + res.author);
+                getQuote = false;
+                $(".loading").css({
+                    "visibility": "hidden",
+                });
+            }
+        });
+    }
 }
